0117123ba702cdb6b8b325470ec0ea7e6e4db153
KAPIQ03 <kapik0324@gmail.com>
2026-03-04 17:28:27 +0100

    implement GET all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import express from 'express';
+import cors from 'cors';
+import subjectRouter from './routes/subjects';
 
 const app = express();
 const PORT = 8000;
 
+app.use(
+	cors({
+		origin: process.env.FRONTEND_URL,
+		methods: ['GET', 'POST', 'PUT', 'DELETE'],
+		credentials: true,
+	}),
+);
+
 app.use(express.json());
+
+app.use('/api/subjects', subjectRouter);
 
 app.get('/', (req, res) => {
 	res.json({ message: 'Classroom Management API is running' });
