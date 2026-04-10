da5bf2fde63ee0d0dc1a7c1e79f5ed29d72407df
KAPIQ03 <kapik0324@gmail.com>
2026-04-10 13:43:52 +0200

    implement security middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import subjectRouter from './routes/subjects';
+import securityMiddleware from './middleware/security';
 
 const app = express();
 const PORT = 8000;
@@ -19,6 +20,7 @@ app.use(
 );
 
 app.use(express.json());
+app.use(securityMiddleware);
 
 app.use('/api/subjects', subjectRouter);
 
